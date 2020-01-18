e5ad557fc79b0d826e91cecafc204fea03f1165a
sjc-bui <bui@setjapan.co.jp>
2020-01-18 22:54:33 +0900

    first commit

---
+++ AppStore/AppStore/Models/Today.cs
@@ -0,0 +1,23 @@
+﻿namespace AppStore.Models
+{
+    /// <summary>
+    /// Defines the <see cref="Today" />
+    /// </summary>
+    public class Today
+    {
+        /// <summary>
+        /// Gets or sets the Category
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Explain
+        /// </summary>
+        public string Explain { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ImageUrl
+        /// </summary>
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ AppStore/AppStore/Views/TodayView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AppStore.Models;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppStore.Views
+{
+    /// <summary>
+    /// Defines the <see cref="TodayView" />
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TodayView : ContentPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TodayView"/> class.
+        /// </summary>
+        public TodayView()
+        {
+            InitializeComponent();
+
+            List<Today> todayData = new List<Today>
+            {
+                new Today { Category = "お気に入り", Explain = "今週おすすめの\nゲーム５選", ImageUrl = "game1.jpeg" },
+                new Today { Category = "おすすめ", Explain = "今日の\nApp", ImageUrl = "game2.jfif" },
+                new Today { Category = "おすすめ", Explain = "今日の\nゲーム", ImageUrl = "game3.jfif" },
+                new Today { Category = "おすすめ", Explain = "今週のApple Arcade\nトップゲーム", ImageUrl = "game4.jfif" }
+            };
+
+            _today.ItemsSource = todayData;
+        }
+    }
+}
